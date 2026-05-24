80aeb641e0099cc70c92423ac1c7b2c32cb63d80
LeviDooku <pedrovelascosantana@gmail.com>
2026-05-24 23:37:57 +0200

    readme

--- src/cuda_inference.cu
+++ src/cuda_inference.cu
@@ -1,17 +1,26 @@
 //Pedro Velasco Santana
 //File: cuda_inference.cu
 
-//Purpose: parallel inference in GPU
+//Purpose: parallel inference in GPU. Each thread processes a different sample
 
 #include <iostream>
 #include "cuda_inference.cuh"
 
+//The neural network is declared as a variable in constant memory.
+//Constant memory is a special zone designed for:
+//1. Small data (the network is small)
+//2. Read-only (none of the weights changes during inference) 
+//3. Shared among threads
+
 __constant__ neural_network constant_network;
 
-__global__ void inference_kernel(const float* entries, float* output, int num_samples){
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
+//Kernel
 
-    if(id < num_samples){
+__global__ void inference_kernel(const float* entries, float* output, int num_samples){
+    int id = blockIdx.x * blockDim.x + threadIdx.x; //Thread id
+
+    //Control limits
+    if(id < num_samples){ 
         int base = id * NUM_INPUTS;
         output[id] = forward_sample(constant_network, &entries[base]);
     }
@@ -24,18 +33,20 @@ void cuda_inference(const neural_network& network, const float* entries, float* 
     int entries_size = num_samples * NUM_INPUTS * sizeof(float);
     int output_size = num_samples * sizeof(float);
 
+    //Reserve memory for inputs and outputs
     cudaMalloc((void**)&device_entries, entries_size);
     cudaMalloc((void**)&device_output, output_size);
 
+    //Copy from CPU to GPU 
     cudaMemcpy(device_entries, entries, entries_size, cudaMemcpyHostToDevice);
 
+    //Copy network to constant_network
     cudaMemcpyToSymbol(constant_network, &network, sizeof(neural_network));
 
     int threads_per_block = 256;
     int blocks_per_grid = (num_samples + threads_per_block - 1) / threads_per_block;
 
-    cudaEvent_t start;
-    cudaEvent_t stop;
+    cudaEvent_t start, stop;
 
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
@@ -49,6 +60,7 @@ void cuda_inference(const neural_network& network, const float* entries, float* 
 
     cudaEventElapsedTime(kernel_time_ms, start, stop);
 
+    //Copy results to CPU
     cudaMemcpy(output, device_output, output_size, cudaMemcpyDeviceToHost);
 
     cudaEventDestroy(start);
